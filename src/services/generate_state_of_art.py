e799d15cc3f08e09b34c00fe89c4c3ffcefd6726
kbauch <karine.bauch.web@gmail.com>
2024-09-26 15:55:08 +0200

    change prompt

--- src/services/generate_state_of_art.py
+++ src/services/generate_state_of_art.py
@@ -10,19 +10,30 @@ def generate(custom_details: str = "") -> str:
     """
     prompt = (
         f"As an Aider developer aiming to master Python automation technologies, this document provides a comprehensive "
-        f"state of the art analysis. {custom_details}"
+        f"state of the art analysis {custom_details}"
     )
+    introduction = services.generate_text.generate(f"{prompt}. Here is an introduction :").replace(prompt, "")
+    synthesis = services.generate_text.generate(f"{prompt}. Here is a global synthesis :").replace(prompt, "")
+    analysis = services.generate_text.generate(f"{prompt}. Here is a critical analysis :").replace(prompt, "")
+    contribution = services.generate_text.generate(f"{prompt}. Here is a proposed contribution :").replace(prompt, "")
+    conclusion = services.generate_text.generate(f"{prompt}. Here is a conclusion :").replace(prompt, "")
 
-    # Call the generate_text function with the prompt
-    generated_text = services.generate_text.generate(prompt)
+    structured_answer = (f"Introduction:\n"
+                         f"{introduction}\n"
+                         f"Global synthesis:\n"
+                         f"{synthesis}\n"
+                         f"Critical analysis:\n"
+                         f"{analysis}\n"
+                         f"Proposed contribution:\n"
+                         f"{contribution}\n"
+                         f"Conclusion:\n"
+                         f"{conclusion}\n").replace(prompt, "")
 
-    structured_text = services.structure_state_of_art.structure(generated_text)
-    return structured_text
+    return structured_answer
 
 
 if __name__ == "__main__":
     # Test the generate_state_of_art_for_aider_developer function
     state_of_art_text = generate()
 
-    print("State of the Art Text for Aider Developer:")
     print(state_of_art_text)
