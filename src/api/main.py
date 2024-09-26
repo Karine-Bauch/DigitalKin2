6463b6cdba7d94dc3098f9d534f7b3d0801f0695
kbauch <karine.bauch.web@gmail.com>
2024-09-26 16:10:39 +0200

    update typing

--- src/api/main.py
+++ src/api/main.py
@@ -7,7 +7,7 @@ app = fastapi.FastAPI()
 @app.get("/state_of_art")
 def state_of_art(custom_details: str = "") -> str:
     try:
-        response = services.generate_state_of_art.generate(custom_details)
+        response: str = services.generate_state_of_art.generate(custom_details)
         return response
     except Exception as e:
         raise fastapi.HTTPException(status_code=500, detail=f"An error occurred: {e}")
